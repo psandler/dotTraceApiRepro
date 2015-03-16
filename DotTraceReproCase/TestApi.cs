4d0149f9ba1c33d0fb47eebc887e26b8e349ba2e
psandler <psandler@performancetrust.com>
2015-03-16 09:53:56 -0500

    working reproduction of the problem

---
+++ DotTraceReproCase/TestApi.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Profiler.Windows.Api;
+using NUnit.Framework;
+
+namespace DotTraceReproCase
+{
+    [TestFixture]
+    public class TestApi
+    {
+        [Test]
+        public void TestTheApi()
+        {
+            if (PerformanceProfiler.IsActive)
+            {
+                PerformanceProfiler.Begin(); //THIS THROWS AN EXCEPTION!!!
+                PerformanceProfiler.Start();
+            }
+
+            if (PerformanceProfiler.IsActive)
+            {
+                PerformanceProfiler.Stop();
+                PerformanceProfiler.EndSave();
+            } 
+        }
+    }
+}
